6cf2467675b7f240072b2403f56286b517aaeb40
kareemlashin <kareemelsayed887@gmail.com>
2020-04-21 08:43:27 +0200

    video and img delete

--- js/main.js
+++ js/main.js
@@ -1,9 +1,20 @@
 
 $("#myfile").on('change',function(){
-    let fileAll=$("#myfile").val()
-    $("#img").css({display:"inline-block"})
+        $("#img").css({display:"inline-block"})
+    $("#img-trash").css({display:"inline-block"})
+
 });
 $("#myfile2").on('change',function(){
-    let fileAll=$("#myfile").val()
     $("#video").css({display:"inline-block"})
+    $("#video-trash").css({display:"inline-block"})
+});
+$("#video-trash").on('click',function(){
+$("#myfile2").val('');
+$("#video").css({display:"none"})
+$("#video-trash").css({display:"none"})
+});
+$("#img-trash").on('click',function(){
+    $("#myfile").val('');
+    $("#img").css({display:"none"})
+    $("#img-trash").css({display:"none"})
 });
